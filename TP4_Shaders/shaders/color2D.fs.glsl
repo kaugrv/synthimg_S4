effcb7757d601edbee5128b05022fb6600994cb6
GERVAIS WENDY <wendy.gervais@heckel02.u-pem.fr>
2023-02-28 15:44:03 +0100

    Fin exo1 du TP4

--- TP4_Shaders/shaders/color2D.fs.glsl
+++ TP4_Shaders/shaders/color2D.fs.glsl
@@ -1,7 +1,37 @@
 #version 330 core
 
 in vec3 vFragColor;
+in vec2 vFragPosition;
 
 out vec3 fFragColor;
+out vec2 fFragPosition;
 
-void main() { fFragColor = vFragColor; };
+void main() {
+  float alpha = 3;
+  float beta = 30.;
+
+  // flou joli
+  float a =
+      alpha * exp(-beta * length(vFragPosition.xy) * length(vFragPosition.xy));
+
+  // repeat
+  a = length(fract(5.0 * vFragPosition));
+
+  // repeat + rond flou
+  a = length(abs(fract(5.0 * vFragPosition) * 2.0 - 1.0));
+
+  // damier rigolo
+  a = mod(floor(10.0 * vFragPosition.x) + floor(10.0 * vFragPosition.y), 2.0);
+
+  // gruyère
+  a = smoothstep(0.3, 0.32, length(fract(5.0 * vFragPosition) - 0.5));
+
+  // briques
+  a = smoothstep(0.4, 0.5,
+                 max(abs(fract(8.0 * vFragPosition.x -
+                               0.5 * mod(floor(8.0 * vFragPosition.y), 2.0)) -
+                         0.5),
+                     abs(fract(8.0 * vFragPosition.y) - 0.5)));
+
+  fFragColor = a * vFragColor;
+}
--- TP4_Shaders/shaders/color2D.vs.glsl
+++ TP4_Shaders/shaders/color2D.vs.glsl
@@ -4,8 +4,28 @@ layout(location = 0) in vec2 aVertexPosition;
 layout(location = 1) in vec3 aVertexColor;
 
 out vec3 vFragColor;
+out vec2 vFragPosition;
+
+mat3 translate(float tx, float ty) {
+  return mat3(vec3(1, 0, tx), vec3(0, 1, ty), vec3(0, 0, 1));
+}
+
+mat3 scale(float sx, float sy) {
+  return mat3(vec3(sx, 0, 0), vec3(0, sy, 0), vec3(0, 0, 1));
+}
+
+mat3 rotate(float a) {
+  float c = cos(radians(a));
+  float s = sin(radians(a));
+
+  return mat3(vec3(c, -s, 0), vec3(s, c, 0), vec3(0, 0, 1));
+}
 
 void main() {
+
   vFragColor = aVertexColor;
-  gl_Position = vec4(aVertexPosition, 0, 1) + vec4(0.5, 0.5, 0, 1);
+  vFragPosition = aVertexPosition;
+
+  gl_Position =
+      vec4((rotate(0.) * scale(2, 2) * vec3(aVertexPosition, 1)).xy, 0, 1);
 };
